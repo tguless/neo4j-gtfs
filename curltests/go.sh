835fb4fcd42725b15872dc98cc95661123441988
Ted Gulesserian <ted.gulesserian@ipsoft.com>
2017-05-20 20:50:12 -0400

    Better build instructions

--- curltests/go.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -H "Content-Type: application/json" -X POST --data @payload1.json http://localhost:8080/customrest/test2?sort=departureTimeInt
--- curltests/go2.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -H "Content-Type: application/json" -X GET  http://localhost:8080/customrest/test
